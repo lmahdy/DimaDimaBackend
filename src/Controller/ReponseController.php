dcf8bb544be8f635dbc5537307ceb6c6e2d4caf2
firas-007 <160600772+firas-007@users.noreply.github.com>
2025-05-01 00:16:15 +0100

    presque sayee

--- src/Controller/ReponseController.php
+++ src/Controller/ReponseController.php
@@ -36,7 +36,7 @@ final class ReponseController extends AbstractController{
     }
 
     #[Route('/new', name: 'app_reponse_new', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can create responses')]
+    // IsGranted annotation removed to allow public access
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         // Get reclamations for the dropdown
@@ -203,7 +203,7 @@ final class ReponseController extends AbstractController{
     }
 
     #[Route('/{id}/edit', name: 'app_reponse_edit', methods: ['GET', 'POST'])]
-    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can edit responses')]
+    // IsGranted annotation removed to allow public access
     public function edit(Request $request, EntityManagerInterface $entityManager, int $id): Response
     {
         // Use a custom query to fetch a single response
@@ -357,7 +357,7 @@ final class ReponseController extends AbstractController{
     }
 
     #[Route('/{id}', name: 'app_reponse_delete', methods: ['POST'])]
-    #[IsGranted('ROLE_ADMIN', message: 'Only administrators can delete responses')]
+    // IsGranted annotation removed to allow public access
     public function delete(Request $request, EntityManagerInterface $entityManager, int $id): Response
     {
         if ($this->isCsrfTokenValid('delete'.$id, $request->getPayload()->getString('_token'))) {
